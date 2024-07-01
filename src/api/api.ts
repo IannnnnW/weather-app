9f08908f9df92dd16a16c912f8414c1209952018
IannnnnW <ianwamai@gmail.com>
2024-07-01 10:15:46 +0300

    resolved an error with https

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,6 @@
 const API: { getGeolocation : (location: string) => Promise<any>, getLocationWeather :(lat :string, lon :string) => Promise<any>, getCurrentDateTime: () => any, getWeatherForNextFiveDays: (lat : string, long : string)=> any} = {
     getGeolocation: async(location)=>{
-        let response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=${import.meta.env.VITE_OPEN_WEATHER_API_KEY}`, { 
+        let response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=${import.meta.env.VITE_OPEN_WEATHER_API_KEY}`, { 
             method: "GET",
         });
         return(await response.json())
